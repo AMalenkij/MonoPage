bc9fc222fffb15d9fa8b270576b20c369128a682
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-11-08 20:14:08 +0100

    Update button.tsx
    add style arrow btn

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
+import React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
+import { ArrowRight } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
@@ -18,12 +18,13 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        arrow: "no-underline disabled:cursor-not-allowed bg-white text-black active:bg-gray-300 rounded-full group/arrow overflow-hidden transition border border-transparent",
       },
       size: {
-        default: "h-10 px-4 py-2 rounded-full",
+        default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10 rounded-full",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
@@ -35,22 +36,48 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  arrow?: boolean
+  children?: React.ReactNode
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, arrow = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+
+    if (arrow) {
+      return (
+        <Comp
+          className={cn(buttonVariants({ variant: "arrow", size, className }))}
+          ref={ref}
+          {...props}
+        >
+          <span className="pl-6 pr-16 py-3 relative inline-flex items-center">
+            <span className="group-hover/arrow:-translate-x-1/3 group-hover/arrow:opacity-0 transition-all duration-200">
+              {children}
+            </span>
+            <div className="flex items-center justify-end absolute inset-x-6">
+              <div className="group-hover/arrow:before:translate-x-0 absolute left-0 right-[2px] flex h-[1.5px] origin-right justify-end overflow-hidden rounded-full before:w-full before:translate-x-[calc(100%-16px)] before:rounded-full before:bg-current before:transition-transform before:duration-200" />
+              <ArrowRight className="text-current" size={12} />
+            </div>
+          </span>
+        </Comp>
+      )
+    }
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {children}
+      </Comp>
     )
   }
 )
+
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
